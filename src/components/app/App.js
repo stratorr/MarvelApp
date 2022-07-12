d33b52c5d04099a08fd7abf17c2f0856dbdac164
Danil Kupriyanov <danilkupriyanov@MacBook-AirM1--Danil.local>
2022-07-12 13:04:56 +0300

    Add router

---
+++ src/components/app/App.js
@@ -0,0 +1,33 @@
+import { lazy, Suspense } from "react";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+
+import AppHeader from "../appHeader/AppHeader";
+import Spinner from "../spinner/Spinner";
+
+const Page404 = lazy(() => import("../pages/404"));
+const MainPage = lazy(() => import("../pages/MainPage"));
+const ComicsPage = lazy(() => import("../pages/ComicsPage"));
+const SingleComicPage = lazy(() => import("../pages/SingleComicPage"));
+
+// 751
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div className="app">
+        <AppHeader />
+        <main>
+          <Suspense fallback={<Spinner />}>
+            <Routes>
+              <Route path="/" element={<MainPage />} />
+              <Route path="/comics" element={<ComicsPage />} />
+              <Route path="/comics/:comicId" element={<SingleComicPage />} />
+              <Route path="*" element={<Page404 />} />
+            </Routes>
+          </Suspense>
+        </main>
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default App;
---
+++ src/components/pages/ComicsPage.js
@@ -0,0 +1,13 @@
+import AppBanner from "../appBanner/AppBaner";
+import ComicsList from "../comicsList/ComicsList";
+
+const ComicsPage = () => {
+  return (
+    <>
+      <AppBanner />
+      <ComicsList />
+    </>
+  );
+};
+
+export default ComicsPage;
